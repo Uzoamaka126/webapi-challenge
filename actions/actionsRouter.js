32e5b2a8d47c836450046580dd7019718806c808
Uzoamaka126 <vanessapeters126@gmail.com>
2019-11-01 12:50:06 +0100

    mvp done

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -5,7 +5,7 @@ const Actions = require('../data/helpers/actionModel');
 const router = express.Router();
 
 // Create a new Action
-router.post('/', (req, res) => {
+router.post('/', validateAction, (req, res) => {
     const newAction = {
         description: req.body.description,
         notes: req.body.notes,
@@ -31,8 +31,57 @@ router
         return res.status(200).json(req.action)
 });
 
+router.delete(':/id', validateProjectId, validateActionId, (req, res) => {
+    Actions.remove(req.action.id)
+    .then(() => {
+        res.status(200).json({
+            message: "This project has been deleted"
+        })
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: "Error deleting project"
+        })
+    })
+});
 
+router.put('/:id', validateProjectId, validateActionId, (req, res) => {
+    Actions.update(req.action.id, req.body)
+    .then(project => {
+        res.status(200).json(project);
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: "Error updating project"
+        })
+    })
+})
 
+function validateProjectId(req, res, next) {
+    const { id } = req.params;
+    if(Number(id) == id) {
+        Projects.getById(id)
+        .then(action => {
+            if(action) {
+                req.action = action;
+                next()
+            } else {
+                res.status(400).json({
+                    message: 'Invalid project id'
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'Something came up when we were checking the project id' + error.message,
+            });
+        });
+    } else {
+        return res.status(400).json({
+            message: 'This id format is wrong'
+        })
+    }
+}
 
 function validateActionId(req, res, next) {
     const { id } = req.params;
